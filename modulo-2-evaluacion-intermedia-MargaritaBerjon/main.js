de1f616bbe6cb83c29c9a8dadb6468f0d7985f61
Margui <margui.berjon@gmail.com>
2020-03-10 09:22:14 +0100

    Number logical add

--- modulo-2-evaluacion-intermedia-MargaritaBerjon/main.js
+++ modulo-2-evaluacion-intermedia-MargaritaBerjon/main.js
@@ -2,9 +2,26 @@ const input = document.querySelector('.js-input');
 const btn = document.querySelector('.js-button');
 const clue = document.querySelector('.js-clue');
 const numberTry = document.querySelector('.js-numbertry');
-const myNumber = 'Mi número aleatorio es ' + getRandomNumber();
+const myNumber = getRandomNumber();
 
 function getRandomNumber() {
   return Math.ceil(Math.random() * 100);
 }
-console.log(myNumber);
+console.log('Mi número aleatorio es ' + myNumber);
+
+function game(number) {
+  if (number === myNumber) {
+    clue.innerHTML = '¡Has ganado campeona!';
+  } else if (number > myNumber) {
+    clue.innerHTML = 'Demasiado alto';
+  } else {
+    clue.innerHTML = 'Demasiado bajo';
+  }
+}
+
+function handlerFunction() {
+  const userNumber = parseInt(input.value);
+  game(userNumber);
+}
+
+btn.addEventListener('click', handlerFunction);
